a74fdf515658a0fc5861f4d3689ed081e29670f4
Mykola-Veres <84578078+Mykola-Veres@users.noreply.github.com>
2022-09-26 18:54:59 +0000

    Deploying to gh-pages from @ Mykola-Veres/finance-ledger@6017ee3b4dcaf8c3a77e58ab1d8fd0b93982d81c 🚀

--- index.4d6bfa15.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=window.innerWidth;console.log("scroll",o);var e=document.querySelector(".header");console.log("headerEl",e.style.opacity),document.addEventListener("scroll",(function(l){var n=window.scrollY;console.log("scroll",n);var r=o<768?120:70;console.log("headerDimensions",r),n>=r&&e.classList.toggle("header-scroll")}))}();
-//# sourceMappingURL=index.4d6bfa15.js.map
--- index.f2dc9749.js
+++
@@ -1,2 +0,0 @@
-const o=window.innerWidth;console.log("scroll",o);const e=document.querySelector(".header");console.log("headerEl",e.style.opacity),document.addEventListener("scroll",(function(l){const n=window.scrollY;console.log("scroll",n);const s=o<768?120:70;console.log("headerDimensions",s),n>=s&&e.classList.toggle("header-scroll")}));
-//# sourceMappingURL=index.f2dc9749.js.map
