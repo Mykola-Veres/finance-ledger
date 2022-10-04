9c8e1ca069f85ec0b3caaf1ab5d734da6a90c7fc
Mykola-Veres <veresnickolay@gmail.com>
2022-10-04 23:59:15 +0300

    add

--- src/js/error.js
+++ src/js/error.js
@@ -2,7 +2,7 @@ const form = document.querySelector('.callback__form');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  const inputs = form.querySelectorAll('input');
+  const inputs = form.querySelectorAll('.callback__input');
   inputs.forEach(input => {
     console.log('input.previousElementSibling', input.previousElementSibling);
     console.log('input.nextElementSibling', input.nextElementSibling);
